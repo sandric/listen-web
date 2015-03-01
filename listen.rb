798f7b7b9260ef0478ece35eeb8b698e9f55899e
sandric <kyrychenkoanton@gmail.com>
2015-03-01 15:13:36 +0200

    implement outputting of setting listeners callback

--- listen.rb
+++ listen.rb
@@ -5,8 +5,38 @@ require "sinatra/streaming"
 set :server, :thin
 set :bind, '0.0.0.0'
 
+module Listen
+  class Record
+    class << self
+      attr_accessor :out
+    end
+
+    def build
+      start = Time.now.to_f
+      @paths = _auto_hash
+
+      # TODO: refactor this out (1 Record = 1 watched dir)
+      listener.directories.each do |directory|
+        _fast_build(directory.to_s)
+      end
+
+      Celluloid::Logger.info "Record.dibuild(): #{Time.now.to_f - start} seconds"
+      Listen::Record.out.puts '<div>All listeners are set.</div>'
+    rescue
+      Celluloid::Logger.warn "build crashed: #{$ERROR_INFO.inspect}"
+      Listen::Record.out.puts '<div>An error happened during setting listeners, look at logs.</div>'
+      raise
+    end
+  end
+end
+
+
+
 get '/' do
   stream do |out|
+
+    Listen::Record.out = out
+
     out.puts <<eos
     <style>
       div {
@@ -80,7 +110,8 @@ eos
 
     out.puts "<div>Starting listening...</div>" 
 
-    listener = Listen.to('/home/sandric/listen-test', debug: true) do |modifications, additions, deletions|
+
+    listener = Listen.to('/home', debug: true) do |modifications, additions, deletions|
       time = Time.now.strftime("%d/%m/%Y %H:%M:%S")
 
       modifications.each { |modification| out.puts "<div class='modified'><span>#{time}</span>#{modification}</div>" }
